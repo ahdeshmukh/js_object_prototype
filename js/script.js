e8598acae6e13289d15d7edb6aabb1da3a32989c
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2016-01-08 15:43:23 -0500

    notes for enumerable, configurable, getters and setters

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 'use strict';
 
 var dog = {name: {first: 'Cable', last: 'Dog'}, breed: 'Lab/Boxer mix'};
-
-Object.defineProperty(dog, 'name', {writable: false});
-dog.name.first = 'Cable123';
-console.log(dog);
+Object.defineProperty(dog, 'fullName', {
+	get: function() {
+		return this.name.first + ' ' + this.name.last;
+	},
+	set: function(value) {
+		var nameParts = value.split(' ');
+		this.name.first = nameParts[0];
+		this.name.last = nameParts[1];
+	}
+});
+display(dog.fullName);
+dog.fullName = 'Muffin Top';
+display(dog.fullName);
+display(dog.name.first);
+display(dog.name.last);
